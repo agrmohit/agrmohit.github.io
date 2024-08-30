48a48ff5383cb9d4948a47fb2538817c74ce8572
Mohit Raj <mohit@agrmohit.com>
2024-08-30 15:07:20 +0530

    feat: fetch github stars in parallel

--- assets/js/script-stars.js
+++ assets/js/script-stars.js
@@ -1,24 +1,41 @@
 // Get GitHub stars and add it next to project title if stars > 0
 async function GitHubStars() {
   const projectArticles = document.querySelectorAll(".github-project");
+  const fetchPromises = []; // Array to store all fetch promises
 
   for (const projectArticle of projectArticles) {
     const h3 = projectArticle.querySelector("h3");
     const githubURL = h3.getAttribute("data-url");
-    let stars = null;
 
+    // Create a promise for each fetch operation
     if (window.location.hostname === "127.0.0.1") {
       // Mock data for local development
-      stars = getMockStars();
+      fetchPromises.push(Promise.resolve(getMockStars())); // Use Promise.resolve to wrap mock data
     } else {
-      stars = await getGithubStars(githubURL);
+      fetchPromises.push(getGithubStars(githubURL)); // Add the fetch promise to the array
     }
+  }
 
-    if (stars) {
-      h3.innerHTML = h3.innerHTML
-        .trim()
-        .replace("<a", ` <span title="${stars} GitHub stars">(${stars}<span class="emoji-rotate">⭐</span>)</span><a`);
-    }
+  try {
+    // Wait for all fetch operations to complete in parallel
+    const starsArray = await Promise.all(fetchPromises);
+
+    // Update the DOM with fetched stars
+    projectArticles.forEach((projectArticle, index) => {
+      const h3 = projectArticle.querySelector("h3");
+      const stars = starsArray[index];
+
+      if (stars) {
+        h3.innerHTML = h3.innerHTML
+          .trim()
+          .replace(
+            "<a",
+            ` <span title="${stars} GitHub stars">(${stars}<span class="emoji-rotate">⭐</span>)</span><a`
+          );
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching GitHub stars:", error);
   }
 
   function getMockStars() {
@@ -31,9 +48,9 @@ async function GitHubStars() {
     const pathname = url.pathname;
     const [owner, repo] = pathname.slice(1).split("/");
     const apiURL = `https://api.github.com/repos/${owner}/${repo}`;
-    const response = await fetch(apiURL);
 
     try {
+      const response = await fetch(apiURL);
       if (!response.ok) {
         const errorData = await response.json();
         console.error(`Error fetching data from GitHub API: ${response.status} ${response.statusText}`);
